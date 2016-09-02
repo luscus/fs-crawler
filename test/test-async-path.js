76788fa72beb5a7cd33f2c63396731dcca231a59
luscus <luscus.redbeard@gmail.com>
2016-09-02 22:17:17 +0200

    fixed stream control and end event

---
+++ test/test-async-path.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var path     = require('path');
+var Crawler  = require('../lib/fs-crawler');
+var testRoot = path.normalize(__dirname + path.sep + 'RESTfs-api-test');
+var options  = {
+  flushOnEnd: true,
+  reverse:    true,
+  noStats:    false,
+  maxDepth:   5
+};
+var crawlerPath = new Crawler();
+crawlerPath.crawlPath(__dirname + '/fs/long/deep/path/to/files', options);
+
+crawlerPath.setEncoding('utf8');
+
+crawlerPath.on('data', function (data) {
+  data = JSON.parse(data);
+
+  console.log('STREAM-DATA-PATH:', data.folderId, '=>', data.path.replace(__dirname, ''));
+});
+
+crawlerPath.on('readable', function () {
+  console.log('STREAM-READABLE', arguments);
+});
+
+crawlerPath.on('end', function (data) {
+  console.log('STREAM-END');
+});
+
+crawlerPath.on('error', function (data) {
+  console.log('STREAM-ERROR', data);
+});
+
+crawlerPath.on('close', function () {
+  console.log('  -- STEAM CLOSED --');
+});
+
+
+process.on('uncaughtException', function (er) {
+  console.error('EXCEPTION', er.stack);
+});
---
+++ test/test-async-tree.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var path     = require('path');
+var Crawler  = require('../lib/fs-crawler');
+var testRoot = path.normalize(__dirname + path.sep + 'RESTfs-api-test');
+var options  = {
+  flushOnEnd: true,
+  reverse:    false,
+  noStats:    false,
+  maxDepth:   0
+};
+
+
+var crawler = new Crawler();
+crawler.crawlTree(testRoot, options);
+
+crawler.setEncoding('utf8');
+
+crawler.on('data', function (data) {
+  data = JSON.parse(data);
+
+  console.log('STREAM-DATA-LEAF:', data.folderId, '=> is folder:', (data.is.directory ? 1 : 0), '=>', data.path.replace(testRoot, ''));
+});
+
+crawler.on('readable', function () {
+  console.log('STREAM-READABLE');
+});
+
+crawler.on('end', function (data) {
+  console.log('STREAM-END => duration');//, data.duration, 'ms, datasets', data.data.length);
+});
+
+crawler.on('error', function (error) {
+  console.log('STREAM-ERROR', error);
+});
+
+crawler.on('close', function () {
+  console.log('  -- STEAM CLOSED --');
+});
